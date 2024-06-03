92a508a940da8f5e7397d06cdb3cdc4b96921550
Daniel Roque Deodato <danielroque2627@gmail.com>
2024-06-03 14:11:41 -0300

    Update led lele.ino

--- Código/led lele/led lele.ino
+++ Código/led lele/led lele.ino
@@ -13,10 +13,4 @@ void loop()
   
   digitalWrite(7, LOW); // desligar led azul
   delay(150);
- 
-  digitalWrite(7, HIGH); // ligar led azul
-  delay(150); 
-  
-  digitalWrite(7, LOW); // desligar led azul
-
 }
