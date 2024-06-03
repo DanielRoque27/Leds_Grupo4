55e940bb7268a87bfb14cd1bcfaf01f8146f21ef
Daniel Roque Deodato <danielroque2627@gmail.com>
2024-06-03 13:33:03 -0300

    Update led soso.ino
    troca de entrada

--- Código/led soso/led soso.ino
+++ Código/led soso/led soso.ino
@@ -3,16 +3,16 @@
 void setup()
 {
  // Declarar as variáveis
-  pinMode(2, OUTPUT); // led vermelho 1
+  pinMode(10, OUTPUT); // led vermelho 1
     
 }
 //Adicionar código "loop", para sempre o semáforo e o fluxo do trânsito continuar.
 
 void loop()
 {
-    digitalWrite(2, HIGH); //acende o led vermelho 
+    digitalWrite(10, HIGH); //acende o led vermelho 
     delay(1000); // Esperar 1 segundo
-    digitalWrite(2, LOW); // apagar o led vermelho
+    digitalWrite(10, LOW); // apagar o led vermelho
     delay (2000);
     
 }
