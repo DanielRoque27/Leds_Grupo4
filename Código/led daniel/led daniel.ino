a5e65c67b4d1221279ac63caa80046618a7af9fe
Daniel Roque Deodato <danielroque2627@gmail.com>
2024-06-03 13:38:44 -0300

    Daniel troca de codigo
    troca de codigo completo

--- Código/led daniel/led daniel.ino
+++ Código/led daniel/led daniel.ino
@@ -9,18 +9,8 @@ void setup()
 
 void loop()
 {
-  delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(led, 1);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(500); // Wait for 1000 millisecond(s)
   digitalWrite(led, 0);
-  delay(1000); // Wait for 1000 millisecond(s)
-  
-  for(int i = 0; i < 5; i++) // repita 5x 
-  {
-    digitalWrite(led, 1);
-    delay(200); // Wait for 200 millisecond(s)
-    digitalWrite(led, 0);
-    delay(200); // Wait for 200 millisecond(s)
-  }
-  
+  delay(500); // Wait for 1000 millisecond(s)
 }
